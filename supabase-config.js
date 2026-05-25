970156e07b4d91e901a0ec611cb0b9892ce8a3b8
tj <2004tjg00@gmail.com>
2026-05-25 07:28:39 -0400

    Dev build

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-// ============================================================
-//  SUPABASE CONFIG
-// ============================================================
-
-const SUPABASE_URL  = 'https://juglanrryqscetbwzyjr.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1Z2xhbnJyeXFzY2V0Ynd6eWpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3OTA2ODAsImV4cCI6MjA5NDM2NjY4MH0.U8v5JpRg-6FvixDxykW6prnUVHzzhw4Mx925xArhkF4';
