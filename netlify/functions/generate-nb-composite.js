fc88f9e5b3531b165f6b660c180cf3a6a1e24e34
tj <2004tjg00@gmail.com>
2026-06-07 23:25:29 -0400

    Dev build

--- netlify/functions/generate-nb-composite.js
+++ netlify/functions/generate-nb-composite.js
@@ -31,7 +31,7 @@ const https  = require('https');
 const crypto = require('crypto');
 
 const LOCATION       = 'us-central1';
-const MODEL          = 'gemini-2.5-flash-image';
+const MODEL          = 'gemini-3.1-flash-image-preview';
 const ANALYSIS_MODEL = 'gemini-2.0-flash-001';
 
 function httpsRequest(options, body) {
